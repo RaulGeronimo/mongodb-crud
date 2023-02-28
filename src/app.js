8e833551b3808a73004e0d8ef035584fe2c05bee
Raul Geronimo <raul090700@gmai.com>
2023-02-27 20:21:58 -0600

    HBS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import indexRoutes from "./routes/indexRoutes";
+import path from "path";
+import {engine} from 'express-handlebars';
 
-const app = express()
+const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine(".hbs", engine({
+    layoutDir: path.join(app.get("views"),"layouts"),
+    defaultLayout: "main",
+    extname: ".hbs"
+}))
+app.set('view engine', '.hbs')
+
+//Rutas
 app.use(indexRoutes);
 
 export default app;
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello Word');
+router.get("/", (req, res) => {
+    res.render('index')
 });
 
-router.get('/about', (req, res) => {
-    res.send('about');
+router.get("/about", (req, res) => {
+    res.render('about')
+});
+
+router.get("/edit", (req, res) => {
+    res.render('edit')
 });
 
 export default router;
