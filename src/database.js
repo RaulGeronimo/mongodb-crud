56375575031044569b31e926dd2c422f841c203b
Raul Geronimo <raul090700@gmai.com>
2023-03-02 17:06:43 -0600

    Conexion

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ import {
 
 (async () => {
     try {
-        const db = await connect("mongodb://localhost:27017/crud-mongo");
+        const db = await connect("mongodb://127.0.0.1:27017/crud-mongo");
         console.log("Conexion exitosa con:", db.connection.name);
     } catch (error) {
         console.error(error);
