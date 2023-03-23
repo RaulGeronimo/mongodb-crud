9df12f9921662e8fa9b773b4d027059aed648b1d
Raul Geronimo <raul090700@gmai.com>
2023-03-23 17:09:22 -0600

    CRUD

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ import { config } from "dotenv";
 
 config();
 
-export const MONGODB_URI = 'mongodb+srv://raul:1829301@task.qvskeun.mongodb.net/?retryWrites=true&w=majority';
-/* export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://raul:1829301@task.qvskeun.mongodb.net/?retryWrites=true&w=majority'; */
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://raul:1829301@task.qvskeun.mongodb.net/?retryWrites=true&w=majority';
 
 export const PORT = process.env.PORT || 3000;
