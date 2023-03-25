04c368eb5ac00ef9b4e3df3d4d17fe50fd497ef4
Raul Geronimo <raul090700@gmai.com>
2023-03-25 16:19:41 -0600

    Mongo-CRUD

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-var _config = require("./config");
-_app["default"].listen(_config.PORT);
-console.log('Servidor en puerto', _config.PORT);
