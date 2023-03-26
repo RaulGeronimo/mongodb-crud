9bac31be6c3894ae6da71552f81eaf7f52f5dc24
Raul Geronimo <raul090700@gmai.com>
2023-03-26 17:17:22 -0600

    Notas

--- dist/config.js
+++ dist/config.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.PORT = exports.MONGODB_URI = void 0;
 var _dotenv = require("dotenv");
 (0, _dotenv.config)();
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://raul:1829301@task.qvskeun.mongodb.net/?retryWrites=true&w=majority';
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/test";
 exports.MONGODB_URI = MONGODB_URI;
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT;
 exports.PORT = PORT;
